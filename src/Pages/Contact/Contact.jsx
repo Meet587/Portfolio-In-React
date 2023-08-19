c42a8e7212e84976e40a446a617bb3a547f1b3cf
Meet587 <meet.rakholiya.hp@gmail.com>
2023-08-20 00:09:08 +0530

    working on contact page

--- src/Pages/Contact/Contact.jsx
+++ src/Pages/Contact/Contact.jsx
@@ -1,69 +1,90 @@
 import React from "react";
-import { Button } from "reactstrap";
+import {
+  Button,
+  Card,
+  CardBody,
+  Col,
+  Container,
+  Form,
+  FormGroup,
+  FormText,
+  Input,
+  Label,
+  Row,
+} from "reactstrap";
 import "./contact.scss";
+import sideImg from '../../assets/img/sidenav2.jpg'
 
 const Contact = () => {
   return (
     <>
       <section id="contact" className="contact sec-pad">
-        <div className="main-container">
-          <h2 className="heading_sec">
-            <span className="heading_2">CONTACT</span>
-            <span className="heading_sec_sub">
+        <Container>
+          <div className="heading_2">CONTACT</div>
+          <div className="heading_text">
+            <p>
               Feel free to Contact me by submitting the form below and I will
               get back to you as soon as possible
-            </span>
-          </h2>
-          <div className="contact-content">
-            <form action="#" className="contact_form" method="post">
-              <div className="contact_form-field">
-                <label htmlFor="name" className="contact_form-label">
-                  Name
-                </label>
-                <input
-                  type="text"
-                  required
-                  placeholder="Enter your name"
-                  className="contact_form-input"
-                  id="name"
-                  name="name"
-                />
-              </div>
-              <div className="contact_form-field">
-                <label htmlFor="email" className="contact_form-label">
-                  Email
-                </label>
-                <input
-                  type="email"
-                  required
-                  placeholder="Enter your email address"
-                  className="contact_form-input"
-                  id="email"
-                  name="email"
-                />
-              </div>
-              <div className="contact_form-field">
-                <label htmlFor="message" className="contact_form-label">
-                  Message
-                </label>
-                <textarea
-                  name="message"
-                  className="contact_form-input"
-                  id="message"
-                  cols="30"
-                  rows="10"
-                  placeholder="Enter your message..."
-                ></textarea>
-              </div>
-              {/* <button type="submit" className="btn btn-submit">
-                SUBMIT
-              </button> */}
-              <Button type="submit" className="btn_primary fw-bold">
-                Submit
-              </Button>
-            </form>
+            </p>
           </div>
-        </div>
+          <div className="contact-content mt-5">
+            <Row>
+              <Col md={6}><img src={sideImg} className='sideImg' /></Col>
+              <Col md={6}>
+                <Card>
+                  <CardBody>
+                    <Form action="#" className="contact_form" method="post">
+                      <FormGroup className="contact_form-field">
+                        <Label htmlFor="name" className="contact_form-label">
+                          Name
+                        </Label>
+                        <Input
+                          type="text"
+                          required
+                          placeholder="Enter your name"
+                          className="contact_form-input"
+                          id="name"
+                          name="name"
+                        />
+                      </FormGroup>
+                      <FormGroup className="contact_form-field">
+                        <Label htmlFor="email" className="contact_form-label">
+                          Email
+                        </Label>
+                        <Input
+                          type="email"
+                          required
+                          placeholder="Enter your email address"
+                          className="contact_form-input"
+                          id="email"
+                          name="email"
+                        />
+                      </FormGroup>
+                      <FormGroup className="contact_form-field">
+                        <Label htmlFor="message" className="contact_form-label">
+                          Message
+                        </Label>
+                        <Input
+                          name="message"
+                          className="contact_form-input"
+                          id="message"
+                          cols="30"
+                          rows="10"
+                          placeholder="Enter your message..."
+                        ></Input>
+                      </FormGroup>
+                      <div className="d-flex justify-content-end">
+                        <Button type="submit" className="btn_primary fw-bold">
+                          Submit
+                        </Button>
+                      </div>
+                    </Form>
+                  </CardBody>
+                </Card>
+              </Col>
+            </Row>
+          </div>
+        </Container>
       </section>
     </>
   );
